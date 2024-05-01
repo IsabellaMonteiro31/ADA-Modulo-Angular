4da5ad29729d18a0bd8186b16634b2c4c5e264b1
Isabella Monteiro <isabellasuplino@gmail.com>
2024-05-01 16:07:53 -0300

    commit2

---
+++ memento-app/.angular/cache/17.3.4/vite/deps/@angular_material_divider.js
@@ -0,0 +1,13 @@
+import {
+  MatDivider,
+  MatDividerModule
+} from "./chunk-ZDLXUFC4.js";
+import "./chunk-BIZDURQT.js";
+import "./chunk-Q36CZYC5.js";
+import "./chunk-72RMAQJG.js";
+import "./chunk-QOHD3WUR.js";
+export {
+  MatDivider,
+  MatDividerModule
+};
+//# sourceMappingURL=@angular_material_divider.js.map
